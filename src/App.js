222ebad8588f1e7b275747af3c86b3fcf941eaeb
UIrich <81659201+UIrich@users.noreply.github.com>
2022-08-09 23:17:58 -0300

    Bug fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 import Webroutes from './Webroutes.js';
 
 function App() {
   return (
     <div className="App">
+      <ChakraProvider>
       <Webroutes />
+      </ChakraProvider>
     </div>
     
   );
