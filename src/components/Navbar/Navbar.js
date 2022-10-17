d42ea66d28331dc12d6ae84b284ef1c9bbaa98ba
UIrich <81659201+UIrich@users.noreply.github.com>
2022-10-17 19:43:36 -0300

    Auth on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,15 +14,69 @@ import {
   MenuList,
   useColorMode,
 } from '@chakra-ui/react';
+import { getToken } from '../../contexts/TokenAuth.jsx';
 import { Link as RouteLink } from "react-router-dom";
-import { QuestionIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { EditIcon, QuestionIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import AvatarImage from '../../assets/duck.svg';
 import Logo from '../../assets/logo.svg';
 import UnknownAvatarImage from '../../assets/grayduck.svg';
 
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
+
+  if(getToken()){
+    return (
+      <Box bg={useColorModeValue('white', 'gray.900')} px={4}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <Box mr='5'>
+          <RouteLink to="/">
+          <Image src={Logo} w="10em"/>
+          </RouteLink>
+          </Box>
+            <Stack
+        flex={{ base: 1, md: 0 }}
+        justify={'flex-end'}
+        direction={'row'}
+        spacing={6}>
+        <Button
+          as={'a'}
+          fontSize={'sm'}
+          fontWeight={400}
+          variant={'link'}
+          href={'/'}>
+          Sair
+        </Button>
+        <Menu>
+            <MenuButton
+              as={Button}
+              rounded={'full'}
+              variant={'link'}
+              cursor={'pointer'}
+              minW={0}>
+              <Avatar
+                size={'sm'}
+                src={AvatarImage}
+              />
+            </MenuButton>
+            <MenuList>
+              <MenuItem icon={<EditIcon/>} as="a" href={`/profile/${getToken().nick}`}>
+              Perfil
+              </MenuItem>
+              <MenuItem icon={<QuestionIcon/>} as="a" href="/help">
+              Central de ajuda
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />} onClick={toggleColorMode}>
+                Modo escuro
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Stack>
+        </Flex>
+    </Box>
+    );
+  } else {
   return (
-    <>
       <Box bg={useColorModeValue('white', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <Box mr='5'>
@@ -81,7 +135,6 @@ export default function Nav() {
           </Stack>
           </Flex>
       </Box>
-
-    </>
-  );
+    );
+  }
 }                                                                               
---
+++ src/contexts/TokenAuth.jsx
@@ -0,0 +1,16 @@
+import Axios from 'axios';
+import { useState } from 'react';
+
+export const Token = 'UserConnection';
+
+export function getToken (){
+    let recup = localStorage.getItem(Token)
+    const user = JSON.parse(recup)
+    return user
+}
+
+export function TestAxios(nick){
+        return (Axios.post(`http://localhost:5000/user/profile`, {
+            nick: nick
+        }))
+}
